bf0f7f5554fdbf035d013f31b31d279a95d47c0f
pranavh002 <pranavh002@gmail.com>
2025-02-15 19:07:01 +0530

    Players Data in data.sql file

---
+++ back-end/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO player (player_id, player_name, player_age, player_type, nationality) VALUES
+(1, 'Virat Kohli', 35, 'Batsman', 'India'),
+(2, 'Sachin Tendulkar', 50, 'Batsman', 'India'),
+(3, 'MS Dhoni', 42, 'Wicketkeeper-Batsman', 'India'),
+(4, 'Steve Smith', 34, 'Batsman', 'Australia'),
+(5, 'Joe Root', 33, 'Batsman', 'England'),
+(6, 'Kane Williamson', 33, 'Batsman', 'New Zealand'),
+(7, 'Babar Azam', 29, 'Batsman', 'Pakistan'),
+(8, 'Pat Cummins', 30, 'Bowler', 'Australia'),
+(9, 'Jasprit Bumrah', 30, 'Bowler', 'India'),
+(10, 'Rashid Khan', 25, 'All-Rounder', 'Afghanistan');
